e199f45de532e0e06f167ba85d67986205be12d4
Victor Ubere <victorubere@gmail.com>
2022-10-30 08:18:16 +0100

    created Links component

---
+++ src/components/Link.jsx
@@ -0,0 +1,32 @@
+
+
+
+export default function Links() {
+  return (
+    <main>
+      <button id="twitter_link">
+        <a href="https://twitter.com/VictorUbere">Twitter Link</a>
+      </button>
+      <button id="btn_zuri">
+        <a href="https://training.zuri.team/">Zuri Team</a>
+      </button>
+      <button id="books">
+        <a href="https:///books.zuri.team">Zuri Books</a>
+      </button>
+      <button id="book_python">
+        <a href="https://books.zuri.team.python-for-beginners?ref_id=Victor_Ubere">
+          Python Books
+        </a>
+      </button>
+      <button id="pitch">
+        <a href="https://background.zuri.teams">Background Check For Coders</a>
+      </button>
+      <button id="book_design">
+        <a href="https://books.zuri.team/design-rules"></a>
+      </button>
+      <button>
+        <a href="">Design Books</a>
+      </button>
+    </main>
+  );
+}
