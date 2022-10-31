22c53bd3283d8a5448c0952f310507a3fa77ec1b
Victor Ubere <victorubere@gmail.com>
2022-10-31 15:43:10 +0100

    id updates

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -5,22 +5,22 @@ export default function Links() {
   return (
     <main>
       <a href="https://twitter.com/VictorUbere">
-        <button id="twitter_link">Twitter Link</button>
+        <button id="twitter__link">Twitter Link</button>
       </a>
       <a href="https://training.zuri.team/">
-        <button id="btn_zuri">Zuri Team</button>
+        <button id="btn__zuri">Zuri Team</button>
       </a>
       <a href="https:///books.zuri.team" title="Find books about design and coding.">
         <button id="books">Zuri Books</button>
       </a>
       <a href="https://books.zuri.team/python-for-beginners?ref_id=Victor_Ubere">
-        <button id="book_python">Python Books</button>
+        <button id="book__python">Python Books</button>
       </a>
       <a href="https://background.zuri.team">
         <button id="pitch">Background Check For Coders</button>
       </a>
       <a href="https://books.zuri.team/design-rules">
-        <button id="book_design">Design Books</button>
+        <button id="book__design">Design Books</button>
       </a>
       <div className="slack_github">
         <img src={slack} alt="slack logo" id="slack"/>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,9 +5,10 @@ export default function Profile() {
     <header className="profile">
       <div>
         <div className="img">
-          <img src={profilePic} alt="victors picture" />
+          <img src={profilePic} alt="victors picture" id="profile__img"/>
         </div>
         <p id="twitter">Victor Ubere</p>
+        <p id="slack" style={{'display': 'none'}}>Victor Ubere</p>
       </div>
     </header>
   );
